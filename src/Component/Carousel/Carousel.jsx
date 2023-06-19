ed4ba145e753b8b989ddbad3f91fbdd6583360c8
OmarGamal0 <128337602+OmarGamal0@users.noreply.github.com>
2023-06-19 20:32:10 +0300

    Add files via upload

--- src/Component/Carousel/Carousel.jsx
+++ src/Component/Carousel/Carousel.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect} from 'react'
+import { useState } from 'react'
 import { DataContext, urlimg } from '../../Context/DataContext'
 import { noxe, noxeActive } from '../../image'
 
@@ -14,28 +15,32 @@ function Carousel() {
     <div className='carousel-item_info'>
     <img src={noxe} alt="" />
     </div>
-    
+
     </div>
     </div>
    
   )
 }
-
 export default Carousel
 
 
 
 let CarouselImg =()=>{
+  let{movies}=useContext(DataContext);
+  const [mo, setmo] = useState(null) ;
 
-    let{movies}=useContext(DataContext);
-    
+  useEffect(() => {
+    let movie =[...movies]
+    setmo(movie);
+  }, [movies])
+
     return(<>
       <div className="carousel-item active">
       <img className='d-block w-100 ' src={noxeActive} alt="" />
       </div>
         
-    {movies ? movies.map((movie ,index)=><div key={index} className="carousel-item ">
-    <img  className='d-block w-100' src={urlimg+movie.poster_path } alt="" />
+    {mo ? mo.map((mo ,index)=><div key={index} className="carousel-item ">
+    <img  className='d-block w-100' src={urlimg+mo.poster_path } alt="" />
     </div>
    ):<></>}
     </>)}
